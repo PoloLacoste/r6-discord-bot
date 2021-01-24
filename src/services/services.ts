c359dd12985ac394a4487e601b0f9ac63d23da16
JM REMEUR <jm@remeur.fr>
2021-01-24 21:18:25 +0100

    update r6-api-caching

--- src/services/services.ts
+++ src/services/services.ts
@@ -1,12 +1,10 @@
 import { container } from "tsyringe";
-import { CacheService, MongoDatabase, R6Service } from "r6-cacher";
+import { CacheService, MongoDatabase, R6Service } from "r6-api-caching";
 
 export function initServices(): void {
 
   container.registerInstance(CacheService, new CacheService(process.env.REDIS_URL));
-
   container.registerInstance(MongoDatabase, new MongoDatabase(process.env.MONGO_URL));
-
   container.registerInstance(R6Service,
     new R6Service(process.env.EMAIL, process.env.PASSWORD, {
       caching: Boolean(JSON.parse(process.env.CACHING || "false")),
